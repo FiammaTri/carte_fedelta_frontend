804db2af6181bb307a48545f8df7428a4253f5c3
FiammaTri <scinty-pcd@live.it>
2025-05-07 18:16:21 +0200

    Css sistemato e modifiche a JS

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,18 +4,8 @@ const container = document.createElement("div");
 container.classList.add("flex-container");
 const cardContainer = document.getElementById("card-container");
 
-var tutte = true;
-
-
-document.addEventListener("DOMContentLoaded", function () {
-if (tutte) {
-    creazioneCard()
-} 
-});
-
 //Creazione delle card
-function creazioneCard(){
-
+document.addEventListener("DOMContentLoaded", function () {
 
     getCards().then(cards => {
         cards.forEach(card => {
@@ -32,45 +22,47 @@ function creazioneCard(){
                 const carta = document.createElement("div");
                 carta.classList.add("carta");
 
+                const logoContainer = document.createElement("div");
+                logoContainer.classList.add("logoContainer");
+
                 const logo = document.createElement("img");
                 logo.classList.add("logo");
                 logo.src = card.store.link;
 
                 const negozio = document.createElement("div");
                 negozio.classList.add("store-name");
-                negozio.textContent = card.store.name;
-
-                /*
+                negozio.textContent = card.store.storeName;
+/*
                 const numero = document.createElement("div"); //utile solo in fase di testing, da rimuovere
                 numero.classList.add("numero");
                 numero.textContent = card.number;
-                */  
+*/
                 // bottone per i dettagli ('div'+'a')
-
-                const div_bottone = document.createElement("div");
-                div_bottone.classList.add("discover_more");
+                
+                //const div_bottone = document.createElement("div");
+                //div_bottone.classList.add("discover_more");
 
                 const link_dettaglio = document.createElement("a");
-                link_dettaglio.classList.add("style_btn");
+                //link_dettaglio.classList.add("style_btn");
                 link_dettaglio.setAttribute("href", "card.html?id=" + card.id);
-                link_dettaglio.textContent = "Dettagli";
-                div_bottone.appendChild(link_dettaglio);
+                //link_dettaglio.textContent = "Dettagli";
+                //div_bottone.appendChild(link_dettaglio);
 
-                carta.appendChild(logo);
+                logoContainer.appendChild(logo);
+                carta.appendChild(logoContainer);
                 carta.appendChild(negozio);
-                carta.appendChild(numero);
-                carta.appendChild(div_bottone);
-
-                container.appendChild(carta);
+               // carta.appendChild(numero);
+                //carta.appendChild(div_bottone);
+                link_dettaglio.appendChild(carta);
+                container.appendChild(link_dettaglio);
+                
 
             }
 
         });
         cardContainer.appendChild(container);
     });
-};
-
-
+});
 
 function getCards() {
     return fetch("http://localhost:8080/api/card", {
@@ -88,9 +80,8 @@ function getCards() {
             document.getElementById("error-message").textContent = "Impossibile caricare le card. Riprova più tardi.";
             return [];
         });
-    }
 
-
+}
 
 function getCardsByName(search_value) {
     return fetch("http://localhost:8080/api/card/store/"+search_value, {
@@ -110,24 +101,17 @@ function getCardsByName(search_value) {
         });
 }
 
-function tutte () {
-    if (!tutte)
-    cardContainer.element.style.visibility = 'hidden';
-}
 
 document.getElementById('ricerca-testo').addEventListener('submit', function (event) {
     event.preventDefault();
-    tutte=false;
-    
+
     const store_search = document.getElementById('tasto-ricerca').value;
     console.log(store_search);
 
     if(store_search==="") {
         alert("Inserire nome store");
-        tutte=true;
         return;
     }
-    tutte();
 
     getCardsByName(store_search).then(cards => {
         cards.forEach(card => {
